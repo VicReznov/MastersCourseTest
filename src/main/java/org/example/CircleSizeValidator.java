0766b7c01158cb1502d6786689fe3641d04d225a
Reznov <reznov03048@gmail.com>
2022-12-06 18:39:23 +0900

    feat: 1단계에서 입력받은 원 크기의 유효성 파악하는 기능 구현

---
+++ src/main/java/org/example/CircleSizeValidator.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class CircleSizeValidator {
+
+    private static final int CIRCLE_SIZE_MAX = 80;
+    private static final int CIRCLE_SIZE_MIN = 1;
+
+    private static final String ERROR_CIRCLE_SIZE_NUMBER = "[ERROR] 숫자를 입력해야 합니다.";
+    private static final String ERROR_CIRCLE_SIZE_RANGE = "[ERROR] 다리 길이는 3~20 이어야 합니다.";
+
+    public void validate(String size) {
+        if (!isNumber(size)) {
+            throw new IllegalArgumentException(ERROR_CIRCLE_SIZE_NUMBER);
+        }
+        validateRange(size);
+    }
+
+    private boolean isNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void validateRange(String input) {
+        int size = Integer.parseInt(input);
+        if (size < CIRCLE_SIZE_MIN || size > CIRCLE_SIZE_MAX) {
+            throw new IllegalArgumentException(ERROR_CIRCLE_SIZE_RANGE);
+        }
+    }
+
+}
